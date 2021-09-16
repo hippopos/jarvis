0e19a097b6125c0aeef2e596b15cd54a5d0e5d9d
Cui Wenchang <wenchang_cui@bizconf.cn>
2021-09-16 11:27:08 +0800

    update client.sh

--- client.sh
+++ client.sh
@@ -1,1 +1,2 @@
-docker run --name jarvis --net=host -d --restart=always  jarvis client --server-addr vpn.k8sfans.com:9999
+#docker run --name jarvis --net=host -d --restart=always  jarvis client --server-addr vpn.k8sfans.com:9999
+./jarvis-darwin client --server-addr vpn.k8sfans.com:9999 --name bigsur --region work
